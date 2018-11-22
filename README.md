cfc5c5b090244999e77d547af602cdedba0d38bd
Francesca Bonin <FBonin@ie.ibm.com>
2018-11-22 14:15:16 +0000

    update swagger documentation and Readme

--- README.md
+++ README.md
@@ -50,9 +50,10 @@ The project takes as input pdfs (scientific articles of behavioural change) and 
 A rest API with a Swagger UI is provided. This allow everybody to test the system from a web browser: [http://23.97.177.82:8180/swagger-ui.html](http://23.97.177.82:8180/swagger-ui.html)
  
 Supported entities for the moment:
+
 Population caractheristics: min age, max age, gender and mean age.
 
-Behavioral Change Intervention: Goal Setting (Behaviour), Problem Solving, Action Planning, Feedback on behaviour, Self-monitoring of behaviour, Social support (unspecified), Information about health consequences, Information about social and Pharmacological support environmental consequences and Reduce negative emotions.
+Behavioural Change Techniques: Goal Setting (Behaviour), Problem Solving, Action Planning, Feedback on behaviour, Self-monitoring of behaviour, Social support (unspecified), Information about health consequences, Information about social and environmental consequences, Pharmacological support and Reduce negative emotions.
 
 
 ## Requirements
@@ -67,40 +68,61 @@ We are releasing:
 - 17 fully annotated open access papers
 
 ## Dataset
-In the context of the [HBCP] (https://www.humanbehaviourchange.org/), 244 papers of behavior science literature have been anontated for 10 behavioral change techniques and 4 population caractheristics ( min age, max age, mean age and gender) according to the BCI Ontology. 
+In the context of the [HBCP] (https://www.humanbehaviourchange.org/), 244 papers of behaviour science literature have been anontated for 10 behavioural change techniques and 4 population characteristic ( min age, max age, mean age and gender) according to the Behaviour Change Intervention Ontology. 
 Our pre-trained model is trained on a subset of 111 papers.
-We release 17 papers (that the model has not been trained on) as a sample dataset. Those 17 papers are open access and publily avaliable.
+We release 17 papers (that the model has not been trained on) as a sample dataset. Those 17 papers are open access and publicly avaliable.
 
 
-## Quickstart
+## Quickstart for behavioural science users
+- Visit this page to demo the system http://23.97.177.82:8180/swagger-ui.html
+- Click on doc-extract-info
+- Choose the entity you want to extract from your pdf:
+     - all BCT present in the paper (allbcts)
+     - all BCT present in the paper using a supervised method (allbcts/supervised)
+     - detect the presence of the bct specified in the parameter "code" (bct)
+     - detect the presence of the bct specified in the parameter "code" using a supervised method (bct/supervised)
+     - the gender of participants (gender)
+     - the minimum age of participants (minage)
+     - the max of participants (maxage)
+     - the mean of participants (meanage)
+- Upload the pdf with the : "choose file" button
+- Click "Try it out!" 
+
+Results will be shown in this format:
+
+```
+code": "Minage",
+  "docName": "Volpp 2009 primary paper.pdf",
+  "extractedValue": "18"
+}
+```
+
+I.E. Looking for gender in pdf Volpp 2009 primary paper.pdf, the extracted value is 18.
+
+
+
+## Quickstart for coders
 
 
 ### Running the project
-To run the project, simply type from your command-line.
-```
-sh scripts/hbcpie.sh
-```
-
-In case you're working on windows, type the following command to index the collection.
+From command line type the following command to index the collection.
 ```
 mvn exec:java@indexer
 ```
-
 The next step is to execute the following command to run the IE pipeline.
 ```
 mvn exec:java@extractor
 ```
 
-
-## Test REST API with Swagger UI
+### Test REST API with Swagger UI
 The project uses spring boot and spring fox with swagger ui. 
 
 From the command line, locate yourself in the project HOME (hbcpIE/) and execute
 ```
 mvn spring-boot:run
 ```
-
 Open a browser and hit this [URL] (http://localhost:8180/swagger-ui.html)
+
 
 ## Java Documentation
 [Javadoc](apidocs/index.html)
@@ -119,5 +141,4 @@ This program is free software; you can redistribute it and/or
 ## Contact
 
 For help or issues using the HBCP code, please submit a GitHub issue.
-
 For personal communication related to the project, please contact the HBCP team (humanbehaviourchange@ucl.ac.uk) 
--- src/main/java/api/SwaggerConfig.java
+++ src/main/java/api/SwaggerConfig.java
@@ -10,18 +10,21 @@ import springfox.documentation.service.Contact;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.License;
 import io.swagger.annotations.SwaggerDefinition;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- *
+ * This class configures the swagger interface for demonstrating the REST APIs.
+ * 
  * @author dganguly
  */
 
@@ -32,25 +35,23 @@ public class SwaggerConfig {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .apiInfo(apiInfo())
-          .select()                                  
+          .select()  
           .apis(RequestHandlerSelectors.any())              
           //.paths(PathSelectors.any())
           .paths(Predicates.not(PathSelectors.regex("/error.*")))
-          .build();                                           
+          .build().useDefaultResponseMessages(false);
     }
     
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
             .title("HBCP Information Extraction API")
-            
             .description("HBCP information extraction APIs allow users to extract information from " +
-"behaviour change literature according to Human Behaviour Change Ontology (HBCO). \n"+ 
-                    "This API has been developed in the context of the HBCP project. Please refer to the project website for any detail: https://www.humanbehaviourchange.org/ \n" +
-"\nUse this page to test APIs directly. \n"
-+ "# Available APIs \n "
-+ "## Extract BCTs \n"
-+ "We provide two APIs (/extract/allbcts, /extract/allbcts/supervised) to extract all BCTs for a given research paper. Currently the following 10 BCTs are supported: \n" +
-                    "* 1.1 Goal setting (behavior)\n" +
+"behaviour change literature according to Human Behaviour Change Ontology (HBCO). \n" +
+"For more information on the implementation and on the dataset used for this API please see: https://github.com/HumanBehaviourChangeProject/Info-extract \n\n" +   
+"##Supported BCTs:\n" +
+"Currently the following 10 BCTs are supported: \n\n" +
+"CODE BCT \n" +
+"* 1.1 Goal setting (behavior)\n" +
 "* 1.2 Problem solving\n" +
 "* 1.4 Action planning\n" +
 "* 2.2 Feedback on behaviour\n" +
@@ -59,15 +60,25 @@ public class SwaggerConfig {
 "* 5.1 Information about health consequences\n" +
 "* 5.3 Information about social and environmental consequences\n" +
 "* 11.1 Pharmacological support\n" +
-"* 11.2 Reduce negative emotions\n " +
-"\n\n Users can also extract a specific BCT using /extract/bcts or /extract/bcts/supervised\n" +
-"by specifying the targeting BCT code (e.g., 1.1 or 1.2). \n\n" +
-"## Extract clinical study population characteristics: \n" +
-"Currently four APIs are provided to extract the gender, maximum age,\n" +
-"mean age, and minimum age information from a given research paper.")
-            .contact(new Contact("HBCP team, IBM Research, Dublin", "https://researcher.watson.ibm.com/researcher/view_group.php?id=8205", "debasis.ganguly1@ie.ibm.com"))
+"* 11.2 Reduce negative emotions\n\n " +
+"#Quickstart:\n\n"+ 
+"* Click on doc-extract-info\n" +
+"* Choose the API according to the entity you want to extract from your pdf:\n" +
+"  * all BCTs present in the paper (allbcts)\n" +
+"  * all BCTs present in the paper using a supervised method (allbcts/supervised)\n" +
+"  * detect the presence of the bct specified in the parameter \"code\" (bct)\n" +
+"  * detect the presence of the bct specified in the parameter \"code\" using a supervised method (bct/supervised)\n" +
+"  * the gender of participants (gender)\n" +
+"  * the minimum age of participants (minage)\n" +
+"  * the max of participants (maxage)\n" +
+"  * the mean of participants (meanage)\n" +
+"  * Upload the pdf with the : \"choose file\" button\n" +
+"* Click \"Try it out!\"\n\n\n"
+//+ "## [HBCP JavaDoc](../apidocs/index.html)"
+             )
+            .contact(new Contact("HBCP team", "https://www.humanbehaviourchange.org/", ""))
             .license("Apache License Version 2.0")
-            .licenseUrl("https://github.ibm.com/Dublin-Research-Lab/hbcpIE")
+            .licenseUrl("https://github.com/HumanBehaviourChangeProject/Info-extract")
             .version("0.1")
             .build();
     }    
